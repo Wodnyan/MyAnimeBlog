67ae5a71cc0a85ba78f266fa74688c66f5568195
Albert Gergo <gergoalbert.gta@gmail.com>
2020-12-12 18:33:56 +0200

    Working on Card, BlogHeader and Header components

--- client/components/Card/Card.tsx
+++ client/components/Card/Card.tsx
@@ -1,7 +1,37 @@
-export default function Card() {
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import styles from "./Card.module.scss";
+import Avatar from "@material-ui/core/Avatar";
+
+export default function BlogPostCard() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Card className={styles.card}>
+      <CardActionArea>
+        <CardMedia
+          className={styles.media}
+          image="https://www.tvovermind.com/wp-content/uploads/2018/12/Gabriel-DropOut.jpeg"
+          title="Bruh"
+        />
+        <CardContent>
+          <Typography>New Gabriel Dropout season announced</Typography>
+          <PostInformation />
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
+
+export const PostInformation = () => {
+  return (
+    <div>
+      <Avatar
+        alt="avatar"
+        src="https://avatars1.githubusercontent.com/u/52217740?s=400&u=ef08f5742fa07f01e340728cef3893126c857938&v=4"
+      />
+      <div></div>
+    </div>
+  );
+};
--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -1,25 +1,35 @@
+import { useState, FormEvent, ChangeEvent } from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Input from "@material-ui/core/Input";
 import styles from "./Header.module.scss";
 import Search from "@material-ui/icons/Search";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
 import InputAdornment from "@material-ui/core/InputAdornment";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Typography variant="h5" component="h1">
-        MyAnimeBlog
-      </Typography>
-      <Nav />
-      <SearchForm />
-      <SignInButton />
+      <AppBar position="fixed">
+        <ToolBar>
+          <Typography variant="h5" component="h1">
+            MyAnimeBlog
+          </Typography>
+          <Nav />
+          <SearchForm />
+          <SignInButton />
+        </ToolBar>
+      </AppBar>
     </header>
   );
 }
 
 export const Nav = () => {
-  const NavListItem = ({ children }) => <li className={styles.navList__item}>{children}</li>;
+  const NavListItem = ({ children }) => (
+    <li className={styles.navList__item}>{children}</li>
+  );
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
@@ -31,15 +41,27 @@ export const Nav = () => {
 };
 
 export const SignInButton = () => (
-  <Button className={styles.button}>Sign in</Button>
+  <Button color="inherit" className={styles.button}>
+    Sign in
+  </Button>
 );
 
 export const SearchForm = () => {
+  const [search, setSearch] = useState("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
   return (
-    <form className={`${styles.searchForm}`}>
+    <form onSubmit={handleSubmit} className={`${styles.searchForm}`}>
       <Input
         placeholder="Search"
-        fullWidth={true}
+        fullWidth
+        style={{ color: "#fff" }}
+        onChange={handleInputChange}
+        value={search}
         startAdornment={
           <InputAdornment position="start">
             <Search />
